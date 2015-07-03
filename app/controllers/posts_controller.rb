0febb669cb2d21260cfa6d7996f87b4749e613d5
Oscar Jurado <oscjurado@gmail.com>
2015-07-02 20:27:18 -0400

    posts controller/model/view in working order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,27 @@
 class PostsController < ApplicationController
+	def create
+		@post = Post.new(post_params)
+
+		@post.save
+
+		redirect_to @post
+	end
+	
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def index
+		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
 	end
+
+	private 
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
+
 end
