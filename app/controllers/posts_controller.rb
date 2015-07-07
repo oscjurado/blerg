36008ea1a71421a6b8566febe574e76a9fff2e5b
Oscar Jurado <oscjurado@gmail.com>
2015-07-07 14:20:06 -0400

    added destroy/edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
+	
 	def create
 		@post = Post.new(post_params)
-
 		if @post.save
 			redirect_to @post
 		else
@@ -19,6 +19,25 @@ class PostsController < ApplicationController
 
 	def new
 		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
 	end
 
 	private 
